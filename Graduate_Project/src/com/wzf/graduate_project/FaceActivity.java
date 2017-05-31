1748187015185d6f11120dba4bb18616f78c0785
w460461339 <wuzifan0817@gmail.com>
2017-05-31 15:24:22 +0100

    use the google-play-service to find facial features.

--- Graduate_Project/src/com/wzf/graduate_project/FaceActivity.java
+++ Graduate_Project/src/com/wzf/graduate_project/FaceActivity.java
@@ -18,6 +18,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.renderscript.ScriptGroup.Input;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -32,36 +33,59 @@ public class FaceActivity extends Activity {
 	private Handler handler = new Handler() {
 		@Override
 		public void handleMessage(android.os.Message msg) {
-			int face_count=(Integer)msg.obj;
-			InfoUtils.showInfo(""+face_count, FaceActivity.this);
+			int face_count = (Integer) msg.obj;
+			InfoUtils.showInfo("" + face_count, FaceActivity.this);
 			drawCircleOnFace(face_count);
 		};
 	};
+	private FacesDisplayView facesDisplayView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// 0. 初始化
-		initFaceActivity(savedInstanceState);
-		// 1. 测试googleplayservice的人脸识别
-		detectFacebyGPS();
+		// 0. 调用父类的方法
+		super.onCreate(savedInstanceState);
+		// 1. 加载相应布局
+		setContentView(R.layout.activity_face);
+		// 2. 得到开启本Activity的intent
+		Intent intent = this.getIntent();
+		// 3. 得到数据
+		Uri uri = (Uri) intent.getParcelableExtra("image");
+		// 4. 得到显示控件1
+		imageView_show = (ImageView) this.findViewById(R.id.iv_show);
+		// 5. 得到显示控件2
+		facesDisplayView = (FacesDisplayView)findViewById(R.id.fdv_detect);
+		// 5. 压缩图片
+		bitmap = ImageUtils.scaleToAndroidImage(uri, FaceActivity.this);
+		// 8. 显示原始图片
+		imageView_show.setImageBitmap(bitmap);
+		
+		
+//		facesDisplayView.setBitmap(bitmap);
 	}
 
+	/**
+	 * google-play-service方法使用
+	 * @param view
+	 */
+	public void click_findFace_playservice(View view){
+		this.facesDisplayView.setBitmap(bitmap);
+	}
+	
 	/**
 	 * 通过googleplayserveic中的api进行人脸识别
 	 */
 	private void detectFacebyGPS() {
 		// 1. 加载自定义view
-		FacesDisplayView facesDisplayView=(FacesDisplayView)findViewById(R.id.fdv_detect);
+		FacesDisplayView facesDisplayView = (FacesDisplayView) findViewById(R.id.fdv_detect);
 		// 2. 加载图片，得到图片的输入流
-		InputStream bInputStream=getResources().openRawResource(R.drawable.myface);
+		InputStream bInputStream = getResources().openRawResource(R.drawable.myface);
 		// 3. 通过输入流得到Bitmap图片
-		Bitmap bitmap=BitmapFactory.decodeStream(bInputStream);
+		Bitmap myBitmap = BitmapFactory.decodeStream(bInputStream);
 		// 4. 将得到的bitmap对象传回
-		facesDisplayView.setBitmap(bitmap);
+		facesDisplayView.setBitmap(myBitmap);
 	}
 
 	private void initFaceActivity(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		// 1. 加载相应布局
 		setContentView(R.layout.activity_face);
@@ -75,7 +99,7 @@ public class FaceActivity extends Activity {
 		bitmap = ImageUtils.scaleToAndroidImage(uri, FaceActivity.this);
 		// 8. 显示图片
 		imageView_show.setImageBitmap(bitmap);
-	}
+	}	
 
 	/**
 	 * 单击，找到人脸
@@ -85,7 +109,7 @@ public class FaceActivity extends Activity {
 	public void click_findFace(View view) {
 		findFaceByOrgAPI();
 	}
-	
+
 	/**
 	 * 利用google原生api来进行人脸识别
 	 */
@@ -101,8 +125,8 @@ public class FaceActivity extends Activity {
 				// 4. 猜测这应该是一个耗时的操作，被放在一个新线程中执行，所以有后面会空指针
 				int face_Count = faceDetector.findFaces(detectImage, faces);
 				// 4. 发送查找到多少张人脸
-				Message message=Message.obtain();
-				message.obj=face_Count;
+				Message message = Message.obtain();
+				message.obj = face_Count;
 				handler.sendMessage(message);
 			}
 		}).start();
@@ -110,13 +134,14 @@ public class FaceActivity extends Activity {
 
 	/**
 	 * 在人脸周围画圈
+	 * 
 	 * @param face_Count
 	 */
 	private void drawCircleOnFace(int face_Count) {
 		// 0. 得到图片的副本
 		Bitmap copyBitmap = ImageUtils.copyOfImage(bitmap);
 		if (face_Count > 0) {
-			for (int i = 0; i <face_Count; i++) {
+			for (int i = 0; i < face_Count; i++) {
 				Face face = this.faces[i];
 				PointF pointF = new PointF();
 				// 5. 拿到眉心位置
@@ -125,21 +150,20 @@ public class FaceActivity extends Activity {
 				int y = (int) pointF.y;
 				// 6. 拿到眼间距
 				int eyeDistance = (int) face.eyesDistance();
-				InfoUtils.showInfo(""+eyeDistance, this);
-				int radio=eyeDistance*9/5;
+				InfoUtils.showInfo("" + eyeDistance, this);
+				int radio = eyeDistance * 9 / 5;
 				for (int xc = -radio; xc <= radio; xc++) {
 					for (int yc = -radio; yc <= radio; yc++) {
-						if (Math.sqrt(xc * xc + yc * yc)<= radio &&
-								Math.sqrt(xc * xc + yc * yc)>= 0.95*radio) {
-							try{
+						if (Math.sqrt(xc * xc + yc * yc) <= radio && Math.sqrt(xc * xc + yc * yc) >= 0.95 * radio) {
+							try {
 								copyBitmap.setPixel(x + xc, y + yc, Color.RED);
-							}catch(Exception exception){
+							} catch (Exception exception) {
 								System.out.println("有时候会画画越界");
 							}
 						}
 					}
 				}
-				
+
 			}
 			this.imageView_show.setImageBitmap(copyBitmap);
 		}
--- Graduate_Project/src/com/wzf/graduate_project/FacesDisplayView.java
+++ Graduate_Project/src/com/wzf/graduate_project/FacesDisplayView.java
@@ -1,55 +1,146 @@
 package com.wzf.graduate_project;
 
+import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
+import com.google.android.gms.vision.face.Landmark;
 import com.wzf.utils.InfoUtils;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 
 public class FacesDisplayView extends View {
 
 	private Bitmap mBitmap;
-	// 这个应该用的是com.google.android.gms.vision.face.Face这个包下的face
 	private SparseArray<Face> mFaces;
 
 	public FacesDisplayView(Context context) {
-		super(context);
+		super(context, null);
 	}
 
 	public FacesDisplayView(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
+		super(context, attrs, 0);
 	}
 
 	public FacesDisplayView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 	}
+	
+	 public void setBitmap( Bitmap bitmap ) {
+	        mBitmap = bitmap;
+	        FaceDetector detector = new FaceDetector.Builder( getContext() )
+	                .setTrackingEnabled(true)
+	                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+	                .setMode(FaceDetector.ACCURATE_MODE)
+	                .build();
 
-	/**
-	 * 将本地mBitmap赋值
-	 * 
-	 * @param bitmap
-	 */
-	public void setBitmap(Bitmap bitmap) {
-		// 1. 给本地图片文件赋值
-		this.mBitmap = bitmap;
-		// 2. 得到检测器(链式构造)
-		FaceDetector faceDetector = new FaceDetector.Builder(getContext()).setTrackingEnabled(false)
-				.setLandmarkType(FaceDetector.ALL_LANDMARKS).setMode(FaceDetector.FAST_MODE).build();
-		// 3. 判断detector可用性
-		if (!faceDetector.isOperational()) {
-			InfoUtils.showInfo("检测不可用", getContext());
-//			while(!faceDetector.isOperational()){
-//				
-//			}
-//			InfoUtils.showInfo("检测可用", getContext());
-		} else {
+	        if (!detector.isOperational()) {
+	            //Handle contingency
+	        } else {
+	            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+	            mFaces = detector.detect(frame);
+	            detector.release();
+	        }
+	        logFaceData();
+	        invalidate();
+	    }
+	 
+	 @Override
+	    protected void onDraw(Canvas canvas) {
+	        super.onDraw(canvas);
 
-		}
-		faceDetector.release();
-	}
+	        if ((mBitmap != null) && (mFaces != null)) {
+	            double scale = drawBitmap(canvas);
+	            drawFaceLandmarks(canvas, scale);
+	            drawFaceBox(canvas, scale);
+	        }
+	    }
+	 private double drawBitmap(Canvas canvas) {
+	        double viewWidth = canvas.getWidth();
+	        double viewHeight = canvas.getHeight();
+	        double imageWidth = mBitmap.getWidth();
+	        double imageHeight = mBitmap.getHeight();
+	        double scale = Math.min(viewWidth / imageWidth, viewHeight / imageHeight);
 
+	        Rect destBounds = new Rect(0, 0, (int)(imageWidth * scale), (int)(imageHeight * scale));
+	        canvas.drawBitmap(mBitmap, null, destBounds, null);
+	        return scale;
+	    }
+	 
+	 private void drawFaceBox(Canvas canvas, double scale) {
+	        //This should be defined as a member variable rather than
+	        //being created on each onDraw request, but left here for
+	        //emphasis.
+	        Paint paint = new Paint();
+	        paint.setColor(Color.GREEN);
+	        paint.setStyle(Paint.Style.STROKE);
+	        paint.setStrokeWidth(5);
+
+	        float left = 0;
+	        float top = 0;
+	        float right = 0;
+	        float bottom = 0;
+
+	        for( int i = 0; i < mFaces.size(); i++ ) {
+	            Face face = mFaces.valueAt(i);
+
+	            left = (float) ( face.getPosition().x * scale );
+	            top = (float) ( face.getPosition().y * scale );
+	            right = (float) scale * ( face.getPosition().x + face.getWidth() );
+	            bottom = (float) scale * ( face.getPosition().y + face.getHeight() );
+
+	            canvas.drawRect( left, top, right, bottom, paint );
+	        }
+	    }
+	 
+	 private void drawFaceLandmarks( Canvas canvas, double scale ) {
+	        Paint paint = new Paint();
+	        paint.setColor( Color.GREEN );
+	        paint.setStyle( Paint.Style.STROKE );
+	        paint.setStrokeWidth( 5 );
+
+	        for( int i = 0; i < mFaces.size(); i++ ) {
+	            Face face = mFaces.valueAt(i);
+
+	            for ( Landmark landmark : face.getLandmarks() ) {
+	                int cx = (int) ( landmark.getPosition().x * scale );
+	                int cy = (int) ( landmark.getPosition().y * scale );
+	                canvas.drawCircle( cx, cy, 10, paint );
+	            }
+
+	        }
+	    }
+	 
+	  private void logFaceData() {
+	        float smilingProbability;
+	        float leftEyeOpenProbability;
+	        float rightEyeOpenProbability;
+	        float eulerY;
+	        float eulerZ;
+	        for( int i = 0; i < mFaces.size(); i++ ) {
+	            Face face = mFaces.valueAt(i);
+
+	            smilingProbability = face.getIsSmilingProbability();
+	            leftEyeOpenProbability = face.getIsLeftEyeOpenProbability();
+	            rightEyeOpenProbability = face.getIsRightEyeOpenProbability();
+	            eulerY = face.getEulerY();
+	            eulerZ = face.getEulerZ();
+
+	            Log.e( "Tuts+ Face Detection", "Smiling: " + smilingProbability );
+	            Log.e( "Tuts+ Face Detection", "Left eye open: " + leftEyeOpenProbability );
+	            Log.e( "Tuts+ Face Detection", "Right eye open: " + rightEyeOpenProbability );
+	            Log.e( "Tuts+ Face Detection", "Euler Y: " + eulerY );
+	            Log.e( "Tuts+ Face Detection", "Euler Z: " + eulerZ );
+	        }
+	    }
 }
